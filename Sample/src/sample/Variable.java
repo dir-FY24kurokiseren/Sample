af253f8019478a550978cb632c1c3dd5a9212c4c
dir-FY24kurokiseren <seren.kuroki.y509@dirbato.co.jp>
2024-04-05 17:05:56 +0900

    ミニ課題

--- Sample/src/sample/Variable.java
+++ Sample/src/sample/Variable.java
@@ -5,11 +5,17 @@ public class Variable {
     public static void main(String[] args) {
         var number1 = 1;
         System.out.println(number1);
+
+        //ミニ課題
         
+        String str1 = "abc";
+        System.out.println(str1);
+        
+        /*
         String str1;
         str1 = "abc";
         System.out.println(str1);
-        
+        */
     }
 
 }
