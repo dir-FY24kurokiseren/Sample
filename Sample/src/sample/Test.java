7c1bd67c53e7bc4cff39fcc59c0317c58412e4c9
dir-FY24kurokiseren <seren.kuroki.y509@dirbato.co.jp>
2024-04-05 16:33:18 +0900

    コミット　main2

--- Sample/src/sample/Test.java
+++ Sample/src/sample/Test.java
@@ -2,10 +2,9 @@ package sample;
 
 public class Test {
 
-    //mainメソッドはプログラムの最初に動作します。
-    public static void main(String[] args) {
+    public static void main2(String[] args) {
         
-        Ｓｙｓｔｅｍ.out.println("Kiramex");
+        System.out.println("Kiramex");
         System.out.println("Java");
         System.out.println("学習を始めましょう!");
         
